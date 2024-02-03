d7c4b6717be06ae5609f145afa02e93cf9555107
Remy Ninganza <remy@ninganzaremy.com>
2024-02-03 14:23:37 -0700

    spring boot app initial jiraSystem set up

---
+++ src/main/java/com/remy/jirastream/JiraStreamApplication.java
@@ -0,0 +1,13 @@
+package com.remy.jirastream;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JiraStreamApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JiraStreamApplication.class, args);
+	}
+
+}
